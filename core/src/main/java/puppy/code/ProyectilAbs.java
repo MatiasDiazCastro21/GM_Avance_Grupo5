0982a48915e8fb8ed23ea074da9ca70ec28f0adc
HantahstarInc <matias.diaz.c01@mail.pucv.cl>
2024-10-30 21:51:57 -0300

    -Archivos comprimidos

--- core/src/main/java/puppy/code/ProyectilAbs.java
+++ core/src/main/java/puppy/code/ProyectilAbs.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.utils.Array;
 
 public abstract class ProyectilAbs {
     public Rectangle hitBox;
